33bfbef52dca49c7c67cfd0625117e365db4f508
inot65 <tft_toni@hotmail.com>
2023-11-27 20:07:11 +0200

    modificari ca sa mearga cu cors()

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const multer = require('multer');
+// const multer = require('multer');
 const path = require('path');
+const cors = require('cors');
 
 // defineste rutele
 const userRoutes = require('./routes/users');
@@ -22,13 +23,28 @@ const app = express();
 // permite parsarea lui body din request
 app.use(express.json());
 
+// folosesc CORS
+app.use(cors());
+
+// setez ceva la MongoDB
+// ce fac la conectare/deconectare MongoDB
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB deconectat...');
+});
+mongoose.connection.on('connected', () => {
+  console.log('Conectat la baza de date!');
+});
+
+const conectareMongoDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 // ma conectez la baza de date
-try {
-  mongoose.connect(process.env.MONGO_URL);
-  console.log('Conectat la baza de date.');
-} catch (error) {
-  console.log('Eroare la conectarea la baza de date...');
-}
+
 
 // ce face asta ?
 // --Explica Bard astfel--
@@ -49,28 +65,28 @@ app.use(helmet());
 app.use(morgan('common'));
 
 // trebuie sa returnez in frontend numele fisierului incarcat
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, './public/images');
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, './public/images');
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, req.body.name);
+//   },
+// });
 
 // folosim multer-ul
-const upload = multer({storage});
+// const upload = multer({storage});
 
 // definesc un nou nod API pt incarcat fisiere
 // important ca numele inputului de fisiere sa fie trecut aici
 // in  upload.single(). La noi este "file"
-app.post('/api/upload', upload.single('file'), (req, res) => {
-  try {
-    return res.status(200).json('File uploaded successufully');
-  } catch (error) {
-    console.log(error);
-  }
-});
+// app.post('/api/upload', upload.single('file'), (req, res) => {
+//   try {
+//     return res.status(200).json('File uploaded successufully');
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 // defineste rutele folosite
 app.use('/api/users', userRoutes);
@@ -80,10 +96,12 @@ app.use('/api/conversations', conversationsRoutes);
 app.use('/api/messages', messagesRoutes);
 
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳 ');
-});
-
 app.listen(8800, () => {
   console.log('Backend server is running on 8800 port...');
+  // conectare la baza de date
+  conectareMongoDb();
+
+  app.get('/', (req, res) => {
+    res.send('Hey this is my API running 🥳 ');
+  });
 });
