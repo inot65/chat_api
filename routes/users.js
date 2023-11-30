c39891248eea5c718b9283cd4edc8d0ef4a9be50
inot65 <tft_toni@hotmail.com>
2023-11-30 19:14:35 +0200

    la update user , trebuie sa returneze userul actualizat

--- routes/users.js
+++ routes/users.js
@@ -21,9 +21,13 @@ router.put('/:id', async (req, res) => {
       }
     }
     try {
-      const user = await User.findByIdAndUpdate(req.params.id, {
-        $set: req.body,
-      });
+      const user = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        {new: true}
+      );
       res.status(200).json('Account has been updated!');
     } catch (error) {
       return res.status(500).json(error);
